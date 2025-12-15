007be938e5c4d891b184dbb9598bb6caa3a2c2a5
Yasmina-R-S <yasiramadan@gmail.com>
2025-12-15 20:14:50 +0100

    1.3
    Hemos puesto el Join después del vFil.Start que hace que el padre espera para que el hijo termine

--- src/cFil/cFilPrincipal.java
+++ src/cFil/cFilPrincipal.java
@@ -9,19 +9,30 @@ public class cFilPrincipal {
 
 		cFil vObjecteFil = new cFil("#1");
 		vObjecteFil.sTemporitzacio(1000); 
+		vObjecteFil.sTemporitzacio(100); 
+
 		// alternativa: innecessari
 		Thread vFil = new Thread(vObjecteFil);
 
 		// alternativa: vObjecteFil
 		vFil.start();
-
-		System.out.println("Iniciant execució procés principal");
+		
+		//1.3
+		//Aquí añadimos el join() → el padre espera al hijo
+		try {
+			vFil.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	System.out.println("Iniciant execució procés principal");
 
 		try {
 
 			for (int vComptador = 0; vComptador < 10; vComptador++) {
 
+
 				Thread.sleep(200); //1.2
+				Thread.sleep(1000); //1.1
 
 				System.out.println("Despertant aturada " + vComptador + " procès principal");
 
@@ -38,5 +49,5 @@ public class cFilPrincipal {
 		System.out.println("Acabant execució procès principal");
 
 	}
-
 }
+
